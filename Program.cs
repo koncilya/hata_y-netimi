255693d7d1d80685dffb7ea6c579a9865435395c
oğuz kaan coşkun <koncilya52@gmail.com>
2022-07-20 15:28:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hata_yönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            try
+            {
+            Console.WriteLine("bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı :" + sayi);
+            
+            }
+            catch(Exception ex)
+            {   
+                Console.WriteLine("Hata: "+ ex.Message.ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+                
+            }
+        }   
+    }
+}
